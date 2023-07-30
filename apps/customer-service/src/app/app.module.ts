576c33ced5a84f84341e6b6a267cd178b46f12d7
marcos <marcosjohanlol@gmail.com>
2023-07-29 21:32:52 -0400

    feat: add prisma to service customer and pilot

--- apps/customer-service/src/app/app.module.ts
+++ apps/customer-service/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from '@puppilots/shared-services';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
