f8512215ebba1fe347960889c43faa6763056f1d
Ricardo Oyarzo Mansilla <raomaster@gmail.com>
2023-08-21 22:48:30 -0400

    Update app.service.ts
    Update app.service.ts

--- apps/customer-service/src/app/app.service.ts
+++ apps/customer-service/src/app/app.service.ts
@@ -1,10 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { Role } from '@prisma/client';
 import { PrismaService } from '@puppilots/shared-services';
 import * as bcrypt from 'bcrypt';
 import { CommonUserDto, CustomerDto, UserLoginDto } from '@puppilots/shared-dtos'
 import { UserExistException, UserNotExistException } from '@puppilots/shared-exceptions'
-
 
 @Injectable()
 export class AppService {
